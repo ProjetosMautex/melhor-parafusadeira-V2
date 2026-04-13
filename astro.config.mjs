62af2117a4db0567c441dd135df1609a700761dd
ProjetosMautex <murilogxmautex@gmail.com>
2026-04-13 17:06:01 -0300

    feat: implement 301 redirect to mestredasferramentas.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  redirects: {
+    '/': 'https://mestredasferramentas.com.br/',
+    '/[...path]': 'https://mestredasferramentas.com.br/[path]'
+  }
+});
